6b69cc9e0fbb54f67542193365074be43c37d1c9
s30175 <alex.biktimirov38@gmail.com>
2025-05-12 17:33:48 +0200

    Initial commit

---
+++ Test1/Services/DbService.cs
@@ -0,0 +1,72 @@
+﻿using System.Data.Common;
+using Microsoft.Data.SqlClient;
+
+namespace Test1.Services;
+
+public class DbService(IConfiguration configuration) : IDbService
+{
+    private readonly string? _connectionString = configuration.GetConnectionString("DefaultConnection");
+
+    // private async Task ValidateData(dto)
+    // {
+    //     
+    // }
+    //
+    // private async Task<int> GetId(int id)
+    // {
+    //     
+    // }
+    //
+    // public async Task<> Get(int id)
+    // {
+    //     var result = new Dto();
+    //     var id = await GetId(id);
+    //
+    //     using (var connection = new SqlConnection(_connectionString))
+    //     {
+    //         await connection.OpenAsync();
+    //         
+    //         var command = new SqlCommand("", connection);
+    //         command.Parameters.AddWithValue("", );
+    //
+    //         return result;
+    //     }
+    // }
+    //
+    // public async Task<> Post(dto)
+    // {
+    //     await ValidateVisit(dto);
+    //
+    //     using (var connection = new SqlConnection(_connectionString))
+    //     {
+    //         await connection.OpenAsync();
+    //
+    //         await using var command = new SqlCommand();
+    //         
+    //         DbTransaction transaction = await connection.BeginTransactionAsync();
+    //         command.Transaction = transaction as SqlTransaction;
+    //
+    //         try
+    //         {
+    //             command.Connection = connection;
+    //             command.CommandText = "";
+    //             command.Parameters.AddWithValue("", );
+    //             command.Parameters.AddWithValue("", );
+    //             command.Parameters.AddWithValue("", );
+    //             
+    //
+    //             await command.ExecuteNonQueryAsync();
+    //
+    //             command.Parameters.Clear();
+    //
+    //             await transaction.CommitAsync();
+    //             return dto;
+    //         }
+    //         catch (Exception)
+    //         {
+    //             await transaction.RollbackAsync();
+    //             throw new ValidationFailedException("Validation of the data has failed");
+    //         }
+    //     }
+    // }
+}
---
+++ Test1/Services/IDbService.cs
@@ -0,0 +1,7 @@
+﻿namespace Test1.Services;
+
+public interface IDbService
+{
+    // public Task<> Get(int id);
+    // public Task<> Post(dto);
+}
